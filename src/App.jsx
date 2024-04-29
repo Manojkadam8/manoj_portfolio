0df10a954eb54bbbf3b7224346c69ae661712c27
Manojkadam8 <kadammanoj7200@gmail.com>
2024-04-30 01:20:30 +0530

    deployt

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <h1>hi</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home.jsx'
 
 
 const router=createBrowserRouter(createRoutesFromElements(
@@ -11,7 +13,9 @@ const router=createBrowserRouter(createRoutesFromElements(
   <Route path='/manoj_portfolio/' element={<Layout />} >
  
  
- <Route path='/manoj_portfolio/app' element={<App />} />
+ <Route path='/manoj_portfolio/' element={<App />} />
+
+ <Route path='/manoj_portfolio/home' element={<Home />} />
 
 
 
