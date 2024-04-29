eccdbdc0ce679c0766c02abcfd26f9572bed95fb
Manojkadam8 <kadammanoj7200@gmail.com>
2024-04-30 01:12:47 +0530

    deployt

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 
 const router=createBrowserRouter(createRoutesFromElements(
